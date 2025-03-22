39af1b3abbf704329aa2099b477d5588ecac1bbf
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-03-22 17:11:57 -0300

    feat: Add command-line flags to run specific exercism functions including Blackjack

--- main.go
+++ main.go
@@ -2,23 +2,44 @@ package main
 
 import (
 	annalyn "exercism/go/annalyns-infiltration"
+	blackjack "exercism/go/blackjack"
 	cars "exercism/go/cars-assemble"
 	greeting "exercism/go/hello-world"
+	lasagna "exercism/go/lasagna"
 	weather "exercism/go/weather-forecast"
 
-	"exercism/go/lasagna"
+	"flag"
 	"fmt"
-	"time"
 )
 
 func main() {
-	exercismHelloWorld()
-	exercismLasagna()
-	exercismAnnalynsInfiltration()
-	exercismCarsAssemble()
-	exercismWeatherForecast()
+	helloWorldFlag := flag.Bool("hello-world", false, "Run the hello-world exercism")
+	annalynsInfiltrationFlag := flag.Bool("annalyns-infiltration", false, "Run the annalyns-infiltration exercism")
+	weatherForecastFlag := flag.Bool("weather-forecast", false, "Run the weather-forecast exercism")
+	carsAssembleFlag := flag.Bool("cars-assemble", false, "Run the cars-assemble exercism")
+	blackJackFlag := flag.Bool("blackjack", false, "Run the blackjack exercism")
+	lasagnaFlag := flag.Bool("lasagna", false, "Run the lasagna exercism")
 
-	fmt.Println(time.Now().Local())
+	flag.Parse()
+
+	if *helloWorldFlag {
+		exercismHelloWorld()
+	}
+	if *annalynsInfiltrationFlag {
+		exercismAnnalynsInfiltration()
+	}
+	if *weatherForecastFlag {
+		exercismWeatherForecast()
+	}
+	if *carsAssembleFlag {
+		exercismCarsAssemble()
+	}
+	if *blackJackFlag {
+		exercismBlackJack()
+	}
+	if *lasagnaFlag {
+		exercismLasagna()
+	}
 }
 
 func exercismLasagna() {
@@ -48,4 +69,9 @@ func exercismCarsAssemble() {
 	fmt.Println(cars.CalculateWorkingCarsPerHour(221, 80))
 	fmt.Println(cars.CalculateWorkingCarsPerMinute(221, 80))
 	fmt.Println(cars.CalculateCost(221))
+}
+
+func exercismBlackJack() {
+	println(blackjack.ParseCard("ace"))
+	println(blackjack.FirstTurn("two", "two", "ace"))
 }
